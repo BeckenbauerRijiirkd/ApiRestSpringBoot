7406c5e48f6b48c209f2bcfb02ffcbe5793ef35f
beckenbauer <you@example.com>
2021-12-07 12:48:12 -0300

    Tratamento de erros

---
+++ src/main/java/br/com/alura/forum/config/ErroDeFormularioDto.java
@@ -0,0 +1,40 @@
+package br.com.alura.forum.config;
+
+public class ErroDeFormularioDto {
+
+    private String campo;
+    private String erro;
+
+    public ErroDeFormularioDto(String campo, String erro) {
+        this.campo = campo;
+        this.erro = erro;
+    }
+    /**
+     * @return String return the campo
+     */
+    public String getCampo() {
+        return campo;
+    }
+
+    /**
+     * @param campo the campo to set
+     */
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    /**
+     * @return String return the erro
+     */
+    public String getErro() {
+        return erro;
+    }
+
+    /**
+     * @param erro the erro to set
+     */
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+
+}
