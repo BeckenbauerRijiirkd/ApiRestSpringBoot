c0ee873a6727c5aff44de0f52bcb141ba01abf67
beckenbauer <you@example.com>
2021-12-07 12:25:22 -0300

    InitialCommit

---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,83 @@
+package br.com.alura.forum.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.model.Topico;
+
+public class TopicoDto {
+    
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriação;
+    
+    public TopicoDto(Topico topico){
+        this.id = topico.getId();
+        this.titulo = topico.getTitulo();
+        this.mensagem = topico.getMensagem();
+        this.dataCriação = topico.getDataCriacao();
+    }
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return String return the mensagem
+     */
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    /**
+     * @param mensagem the mensagem to set
+     */
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    /**
+     * @return LocalDateTime return the dataCriação
+     */
+    public LocalDateTime getDataCriação() {
+        return dataCriação;
+    }
+
+    /**
+     * @param dataCriação the dataCriação to set
+     */
+    public void setDataCriação(LocalDateTime dataCriação) {
+        this.dataCriação = dataCriação;
+    }
+
+    public static List<TopicoDto> converter(List<Topico> topicos){
+        return topicos.stream().map(TopicoDto:: new).collect(Collectors.toList());
+    }
+
+}
