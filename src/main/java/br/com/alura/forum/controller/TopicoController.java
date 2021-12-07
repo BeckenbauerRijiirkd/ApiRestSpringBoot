8358eb3258628328628fcaab641c59e43a587ed3
beckenbauer <you@example.com>
2021-12-07 15:51:18 -0300

    Detalhando topicos

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,22 +1,21 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.DetalhesDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
@@ -60,6 +59,15 @@ public class TopicoController {
         URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 
         return(ResponseEntity.created(uri).body(new TopicoDto(topico)));
+    }
+
+    @GetMapping("/{id}")
+    public DetalhesDto detalhar(@PathVariable Long id) {
+        
+        Topico topico = topicoRepository.getById(id);
+        
+        return new DetalhesDto(topico);
+
     }
 
 }
