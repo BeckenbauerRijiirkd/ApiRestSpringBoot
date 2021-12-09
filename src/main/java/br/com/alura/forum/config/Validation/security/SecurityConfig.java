de25d9db940d4fcb70e6488632d7c06e481ff65e
beckenbauer <you@example.com>
2021-12-09 15:16:13 -0300

    Authetication STATELESS

--- src/main/java/br/com/alura/forum/config/Validation/security/SecurityConfig.java
+++ src/main/java/br/com/alura/forum/config/Validation/security/SecurityConfig.java
@@ -1,14 +1,20 @@
 package br.com.alura.forum.config.Validation.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.alura.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
@@ -16,6 +22,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
     @Autowired
     private AutenticacaoService autenticacaoService;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -27,10 +46,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         
         http.authorizeRequests()
-        .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-        .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+
+        .antMatchers(HttpMethod.POST, "/auth").permitAll()
         .anyRequest().authenticated()
-        .and().formLogin();
+        .and().csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().addFilterBefore(new AutenticacaoTokenFilter(tokenService, repository), UsernamePasswordAuthenticationFilter.class);
+    
     }
 
     @Override
---
+++ src/main/java/br/com/alura/forum/controller/dto/TokenDto.java
@@ -0,0 +1,42 @@
+package br.com.alura.forum.controller.dto;
+
+public class TokenDto {
+
+    private String token;
+    private String tipo;
+
+    public TokenDto(String token, String tipo) {
+        this.token = token;
+        this.tipo = tipo;
+    }
+    
+
+    /**
+     * @return String return the token
+     */
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * @param token the token to set
+     */
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    /**
+     * @return String return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+}
