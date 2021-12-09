be24339908b6964193bd5d1203582da9b75cab10
beckenbauer <you@example.com>
2021-12-09 08:48:23 -0300

    Order

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -42,11 +44,8 @@ public class TopicoController {
 
     @GetMapping
     public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
-                                 @RequestParam int pagina,
-                                 @RequestParam int qtd){
-        
-        Pageable paginacao = PageRequest.of(pagina, qtd);
-
+                                 @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10)
+                                  Pageable paginacao){
 
         if (nomeCurso == null) {
 
