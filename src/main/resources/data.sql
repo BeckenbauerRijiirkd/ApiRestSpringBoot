499036f976f692ef95d313aee9867524b124cb24
beckenbauer <you@example.com>
2021-12-13 11:55:24 -0300

    Autorização

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,13 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$hdyFo/z9JKdxGafKM2uxNuijgmQXQiu2eC1MFZAQ.lrXPbcy8D10.');
 
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$hdyFo/z9JKdxGafKM2uxNuijgmQXQiu2eC1MFZAQ.lrXPbcy8D10.');
+
+INSERT INTO PERFIL(id, nome) VALUES(1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome) VALUES(2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(1, 1);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(2, 2);
+
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programacao');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Frontend');
 
